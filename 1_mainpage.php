84c02704522411bc4c4554f5c77db3dd2081f22b
quintonsteele <47757467+quintonsteele@users.noreply.github.com>
2019-04-06 20:38:23 -0400

    Add files via upload

---
+++ 1_mainpage.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Conference Gate</title>
+</head>
+<body>
+	Welcome to Conference Gate!<br />What would you like to do?
+	<form>
+	<input type="button" value="Register" onclick="window.location.href='2_userregistration.php'" />
+	<input type="button" value="Login" onclick="window.location.href='3_login.php'" />
+	<input type="button" value="Search Conferences" onclick="window.location.href='4_conferencesearch.php'" />
+	<input type="button" value="Register for a Conference" onclick="window.location.href='13_conferenceregistration.php'" />
+	</form>
+</body>
+</html>
